dbf117584715be2fa29bf92ec7ae822ce9714758
M.TahaBilal <tahab9044@gmail.com>
2026-04-17 19:55:32 +0500

    update payment controlle

--- backend/src/controller/paymentController.ts
+++ backend/src/controller/paymentController.ts
@@ -7,35 +7,26 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     const { items } = req.body;
 
-    if (!items || items.length === 0) {
-      return res.status(400).json({ message: "No items provided" });
-    }
-
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-
       line_items: items.map((item: any) => ({
         price_data: {
           currency: "usd",
           product_data: {
             name: item.name,
+            description: `Size: ${item.size || "N/A"} | Color: ${item.color || "N/A"}`,
           },
-          unit_amount: item.price * 100, // cents
+          unit_amount: item.price * 100,
         },
         quantity: item.quantity,
       })),
-
       success_url: "http://localhost:3000/success",
       cancel_url: "http://localhost:3000/cancel",
     });
 
-    return res.status(200).json({
-      url: session.url,
-    });
-
+    return res.json({ url: session.url });
   } catch (error: any) {
-    console.error("Stripe Error:", error.message);
-    return res.status(500).json({ message: "Payment failed" });
+    return res.status(500).json({ message: error.message });
   }
 };
